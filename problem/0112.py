2e1a8d0a88263dbf238bbdc4fa4d2c2fcb9ea021
RegiusQuant <gilgemesh2012@gmail.com>
2020-07-07 22:00:09 +0800

    update problem

---
+++ problem/0112.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/7 下午9:56
+# @Author  : RegiusQuant <315135833@qq.com>
+# @Project : leetcode
+# @File    : 0112.py
+# @Desc    : 说明
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right and sum == root.val:
+            return True
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
